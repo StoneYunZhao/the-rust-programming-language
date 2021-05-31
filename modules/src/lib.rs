275a49e7d93441e31fea381d5f7dba9312477a45
StoneYunZhao <zhaoyun2316@gmail.com>
2021-05-31 20:44:17 +0800

    modules

---
+++ modules/src/lib.rs
@@ -0,0 +1,9 @@
+pub use crate::front_of_house::hosting;
+
+mod front_of_house;
+
+pub fn eat_at_restaurant() {
+    hosting::add_to_waitlist();
+    hosting::add_to_waitlist();
+    hosting::add_to_waitlist();
+}
---
+++ restaurant/src/lib.rs
@@ -0,0 +1,32 @@
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
+
+
+mod front_of_house {
+    pub mod hosting {
+        pub fn add_to_waitlist() {}
+    }
+
+    mod serving {
+        fn serve_order() {}
+
+        mod back_of_house {
+            fn fix_incorrect_order() {
+                super::serve_order();
+            }
+        }
+    }
+}
+
+pub fn eat_at_restaurant() {
+    // Absolute path
+    crate::front_of_house::hosting::add_to_waitlist();
+
+    // Relative path
+    front_of_house::hosting::add_to_waitlist();
+}
